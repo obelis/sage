0a0e7bb6db3887ae85ad8f5428e9eeaad2883018
Mike <mikeh@ioprint.com>
2015-08-06 17:41:44 -0400

    test

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_template_part('templates/page', 'header'); ?>
-
+<!-- did this work?? --->
 <div class="alert alert-warning">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'sage'); ?>
 </div>
